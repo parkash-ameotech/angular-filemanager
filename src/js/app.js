a6644434b3060871c27a075aa1b5f6a4bb2aa52a
Fedor Losev <fedor.losev@thetalabs.net>
2017-03-02 10:01:03 +0200

    ng upload added

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 (function(window, angular, $) {
     'use strict';
-    angular.module('FileManagerApp', ['pascalprecht.translate', 'angularFileUpload']);
+    angular.module('FileManagerApp', ['pascalprecht.translate', 'ngFileUpload', 'angularFileUpload']);
 
     /**
      * jQuery inits
