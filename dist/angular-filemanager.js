3d3de8dd06b24d83ef4979e79ec2af899a63b35e
Fedor Losev <fedor.losev@thetalabs.net>
2017-03-21 10:43:14 +0200

    editing is disconnected

--- dist/angular-filemanager.js
+++ dist/angular-filemanager.js
@@ -57,6 +57,47 @@
  
 })(window, angular, jQuery);
 
+(function(angular) {
+    'use strict';
+    var app = angular.module('FileManagerApp');
+
+    app.directive('angularFilemanager', ['$parse', 'fileManagerConfig', function($parse, fileManagerConfig) {
+        return {
+            restrict: 'EA',
+            templateUrl: fileManagerConfig.tplPath + '/main.html'
+        };
+    }]);
+
+    app.directive('ngFile', ['$parse', function($parse) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var model = $parse(attrs.ngFile);
+                var modelSetter = model.assign;
+
+                element.bind('change', function() {
+                    scope.$apply(function() {
+                        modelSetter(scope, element[0].files);
+                    });
+                });
+            }
+        };
+    }]);
+
+    app.directive('ngRightClick', ['$parse', function($parse) {
+        return function(scope, element, attrs) {
+            var fn = $parse(attrs.ngRightClick);
+            element.bind('contextmenu', function(event) {
+                scope.$apply(function() {
+                    event.preventDefault();
+                    fn(scope, {$event: event});
+                });
+            });
+        };
+    }]);
+    
+})(angular);
+
 (function(angular, $) {
     'use strict';
     angular.module('FileManagerApp').controller('FileManagerCtrl', [
@@ -201,7 +242,7 @@
             }
             
             if (item.isEditable()) {
-                return $scope.openEditItem(item);
+                //return $scope.openEditItem(item);
             }
         };
 
@@ -509,47 +550,6 @@
         };
 
     }]);
-})(angular);
-
-(function(angular) {
-    'use strict';
-    var app = angular.module('FileManagerApp');
-
-    app.directive('angularFilemanager', ['$parse', 'fileManagerConfig', function($parse, fileManagerConfig) {
-        return {
-            restrict: 'EA',
-            templateUrl: fileManagerConfig.tplPath + '/main.html'
-        };
-    }]);
-
-    app.directive('ngFile', ['$parse', function($parse) {
-        return {
-            restrict: 'A',
-            link: function(scope, element, attrs) {
-                var model = $parse(attrs.ngFile);
-                var modelSetter = model.assign;
-
-                element.bind('change', function() {
-                    scope.$apply(function() {
-                        modelSetter(scope, element[0].files);
-                    });
-                });
-            }
-        };
-    }]);
-
-    app.directive('ngRightClick', ['$parse', function($parse) {
-        return function(scope, element, attrs) {
-            var fn = $parse(attrs.ngRightClick);
-            element.bind('contextmenu', function(event) {
-                scope.$apply(function() {
-                    event.preventDefault();
-                    fn(scope, {$event: event});
-                });
-            });
-        };
-    }]);
-    
 })(angular);
 
 (function(angular) {
