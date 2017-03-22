11101bc7daef0acd3022ab09358b24db92c404af
Fedor Losev <fedor.losev@thetalabs.net>
2017-03-22 11:47:08 +0200

    in progress

--- dist/angular-filemanager.js
+++ dist/angular-filemanager.js
@@ -1,6 +1,6 @@
 (function(window, angular, $) {
     'use strict';
-    angular.module('FileManagerApp', ['pascalprecht.translate', 'angularFileUpload']);
+    angular.module('FileManagerApp', ['pascalprecht.translate', 'ngFileUpload', 'angularFileUpload']);
 
     /**
      * jQuery inits
@@ -730,6 +730,53 @@
         return Item;
     }]);
 })(angular);
+(function(angular) {
+    'use strict';
+    var app = angular.module('FileManagerApp');
+
+    app.filter('strLimit', ['$filter', function($filter) {
+        return function(input, limit, more) {
+            if (input.length <= limit) {
+                return input;
+            }
+            return $filter('limitTo')(input, limit) + (more || '...');
+        };
+    }]);
+
+    app.filter('fileExtension', ['$filter', function($filter) {
+        return function(input) {
+            return /\./.test(input) && $filter('strLimit')(input.split('.').pop(), 3, '..') || '';
+        };
+    }]);
+
+    app.filter('formatDate', ['$filter', function() {
+        return function(input) {
+            return input instanceof Date ?
+                input.toISOString().substring(0, 19).replace('T', ' ') :
+                (input.toLocaleString || input.toString).apply(input);
+        };
+    }]);
+
+    app.filter('humanReadableFileSize', ['$filter', 'fileManagerConfig', function($filter, fileManagerConfig) {
+      // See https://en.wikipedia.org/wiki/Binary_prefix
+      var decimalByteUnits = [' kB', ' MB', ' GB', ' TB', 'PB', 'EB', 'ZB', 'YB'];
+      var binaryByteUnits = ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+
+      return function(input) {
+        var i = -1;
+        var fileSizeInBytes = input;
+
+        do {
+          fileSizeInBytes = fileSizeInBytes / 1024;
+          i++;
+        } while (fileSizeInBytes > 1024);
+
+        var result = fileManagerConfig.useBinarySizePrefixes ? binaryByteUnits[i] : decimalByteUnits[i];
+        return Math.max(fileSizeInBytes, 0.1).toFixed(1) + ' ' + result;
+      };
+    }]);
+})(angular);
+
 /*
  angular-file-upload v1.1.5
  https://github.com/nervgh/angular-file-upload
@@ -3513,53 +3560,6 @@ module
             download_as_zip: 'Scarica come file ZIP'
         });
 
-    }]);
-})(angular);
-
-(function(angular) {
-    'use strict';
-    var app = angular.module('FileManagerApp');
-
-    app.filter('strLimit', ['$filter', function($filter) {
-        return function(input, limit, more) {
-            if (input.length <= limit) {
-                return input;
-            }
-            return $filter('limitTo')(input, limit) + (more || '...');
-        };
-    }]);
-
-    app.filter('fileExtension', ['$filter', function($filter) {
-        return function(input) {
-            return /\./.test(input) && $filter('strLimit')(input.split('.').pop(), 3, '..') || '';
-        };
-    }]);
-
-    app.filter('formatDate', ['$filter', function() {
-        return function(input) {
-            return input instanceof Date ?
-                input.toISOString().substring(0, 19).replace('T', ' ') :
-                (input.toLocaleString || input.toString).apply(input);
-        };
-    }]);
-
-    app.filter('humanReadableFileSize', ['$filter', 'fileManagerConfig', function($filter, fileManagerConfig) {
-      // See https://en.wikipedia.org/wiki/Binary_prefix
-      var decimalByteUnits = [' kB', ' MB', ' GB', ' TB', 'PB', 'EB', 'ZB', 'YB'];
-      var binaryByteUnits = ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
-
-      return function(input) {
-        var i = -1;
-        var fileSizeInBytes = input;
-
-        do {
-          fileSizeInBytes = fileSizeInBytes / 1024;
-          i++;
-        } while (fileSizeInBytes > 1024);
-
-        var result = fileManagerConfig.useBinarySizePrefixes ? binaryByteUnits[i] : decimalByteUnits[i];
-        return Math.max(fileSizeInBytes, 0.1).toFixed(1) + ' ' + result;
-      };
     }]);
 })(angular);
 
